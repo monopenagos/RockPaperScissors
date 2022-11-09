c38a069d89d48afa2addbcd0944509e3361963b0
monopenagos <juancpenagos@gmail.com>
2022-11-09 17:33:03 -0500

    Game rounds implemented and prompt control ready

--- js/rockpaperscissors.js
+++ js/rockpaperscissors.js
@@ -1,47 +1,37 @@
+let computerScore = 0;
+let playerScore = 0;
+let gameRound = 1;
+
 function getComputerChoice() {
-  // let ComputerChoice;
   let ComputerChoiceNumber = Math.floor(Math.random() * 3 + 1);
-  // console.log("computer ", ComputerChoiceNumber);
-  // switch (ComputerChoiceNumber) {
-  //   case 1:
-  //     ComputerChoice = "Rock";
-  //     break;
-  //   case 2:
-  //     ComputerChoice = "Paper";
-  //     break;
-  //   case 3:
-  //     ComputerChoice = "Scissors";
-  //     break;
-  // }
   return ComputerChoiceNumber;
 }
 
 function getPlayerChoice() {
-  // let PlayerChoice;
-  let PlayerChoiceNumber = parseInt(
-    prompt("What is your choice?\n1-Rock\n2-Paper\n3-Scissors")
-  );
-  // switch (PlayerChoiceNumber) {
-  //   case 1:
-  //     PlayerChoice = "Rock";
-  //     break;
-  //   case 2:
-  //     PlayerChoice = "Paper";
-  //     break;
-  //   case 3:
-  //     PlayerChoice = "Scissors";
-  //     break;
-  // }
-  // console.log("player ", PlayerChoiceNumber);
-  return PlayerChoiceNumber;
+  let playerSelection = false;
+  while (!playerSelection) {
+    let PlayerChoiceNumber = parseInt(
+      prompt("What is your choice?\n1-Rock\n2-Paper\n3-Scissors")
+    );
+    if (isNaN(PlayerChoiceNumber)) {
+      console.log("Please select within the options");
+    } else if (PlayerChoiceNumber < 1 || PlayerChoiceNumber > 3) {
+      console.log("Please select within the options");
+    } else {
+      console.clear();
+      playerSelection = true;
+      return PlayerChoiceNumber;
+    }
+  }
+  // return PlayerChoiceNumber; <-acá no funciona
 }
 
 function playRound() {
   const gameOptions = ["Rock", "Paper", "Scissors"];
   let ComputerChoice = getComputerChoice();
-  console.log("player ", ComputerChoice);
+  // console.log("Computer ", ComputerChoice);
   let PlayerChoice = getPlayerChoice();
-  console.log("player ", PlayerChoice);
+  // console.log("Player ", PlayerChoice);
   console.log("Computer choose", gameOptions[ComputerChoice - 1]);
   console.log("Player choose", gameOptions[PlayerChoice - 1]);
   switch (true) {
@@ -50,32 +40,60 @@ function playRound() {
       break;
     case ComputerChoice == 1 && PlayerChoice == 2:
       console.log("You win! Paper beats Rock!");
+      playerScore++;
       break;
     case ComputerChoice == 1 && PlayerChoice == 3:
       console.log("You loose! Rock beats Scissors!");
+      computerScore++;
       break;
     case ComputerChoice == 2 && PlayerChoice == 1:
       console.log("You loose! Paper beats Rock!");
+      computerScore++;
       break;
     case ComputerChoice == 2 && PlayerChoice == 3:
       console.log("You win! Scissors beats Paper!");
+      playerScore++;
       break;
     case ComputerChoice == 3 && PlayerChoice == 1:
       console.log("You win! Rock beats Scissor!");
+      playerScore++;
       break;
     case ComputerChoice == 3 && PlayerChoice == 2:
       console.log("You loose! Scissors beats Paper!");
+      computerScore++;
       break;
   }
 }
+
 function game() {
+  console.clear();
   for (let i = 0; i < 5; i++) {
     playRound();
+    displayScore();
+  }
+  finalScore();
+}
+
+function displayScore() {
+  console.log(
+    "Round = ",
+    gameRound,
+    "| Player =",
+    playerScore,
+    "| Computer =",
+    computerScore
+  );
+  gameRound++;
+}
+
+function finalScore() {
+  if (computerScore == playerScore) {
+    console.log("It's a draw!!");
+  } else if (computerScore > playerScore) {
+    console.log("Computer wins... you loose!!");
+  } else {
+    console.log("Player wins!!");
   }
 }
 
 game();
-
-// getPlayerChoice();
-
-// console.log(getComputerChoice());
